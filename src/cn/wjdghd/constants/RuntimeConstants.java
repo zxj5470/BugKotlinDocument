c6dc88ecbb0fa63f1ea522557b70e105b2bf409f
zxj5470 <zxj5470@foxmail.com>
2017-07-30 22:28:32 +0800

    v0.1.3 replace to RuntimeConstants.java

---
+++ src/cn/wjdghd/constants/RuntimeConstants.java
@@ -0,0 +1,13 @@
+package cn.wjdghd.constants;
+
+/**
+ * Created by Administrator on 2017/7/30 0030.
+ */
+public class RuntimeConstants {
+    public static final String NEXT_LINE="\n";
+    public static final String PARAM="@param";
+    public static final String RETURN="@return";
+    public static final String DOC_INNER=" * ";
+    public static final String LINE_SPLIT_COLON=" : ";
+    public static final String DOC_END=" */";
+}
